882e1926226f084d018512695ed862d33b1cf25d
Zach Retterath <ztretterath@gmail.com>
2016-11-10 10:38:03 -0500

    Create user schema

---
+++ models/character.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var CharacterSchema = require('./character').schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  username: String,
+  password: String,
+  characters: [CharacterSchema],
+  createdAt: Date,
+  updatedAt: Date
+})
+
+UserSchema.plugin(passportLocalMongoose);
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
